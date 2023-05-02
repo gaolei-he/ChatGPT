edd3859e9b32eddf52a2932ac7f15cb4c5844482
whoami096 <ahegaolei@163.com>
2023-05-02 10:16:10 +0800

    update

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import socket
 
+
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ip = input("请输入服务器IP地址：")
     client_socket.connect((ip, 3389))
-    passwd = input('请输入连接密码：')
+    passwd = input(client_socket.recv(4096).decode())
     client_socket.send(passwd.encode())
     status = client_socket.recv(4096).decode()
-    
+
     if status.startswith('连接成功'):
         print(status)
     else:
@@ -15,9 +16,9 @@ def main():
         client_socket.close()
         return
 
-
     try:
         while True:
+            
             data = input()
             client_socket.send(data.encode())
 
@@ -25,11 +26,14 @@ def main():
             if data == "quit" or data == "exit":
                 break
             print(result)
+
+            print(client_socket.recv(4096).decode())
         client_socket.send("quit".encode())
     except:
         client_socket.send("quit".encode())
 
     client_socket.close()
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -12,22 +12,22 @@ import time
 
 def Chat(ask: str, message_history: list[dict]) -> str:
     '''处理当前问题
-    
+
     返回值：
         ChatGPT的回答
     '''
 
     message = MakeMessages(ask, message_history)
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = message
+        model="gpt-3.5-turbo",
+        messages=message
     )
     response_content = response.choices[0].message.content
     message_history.append((ask, response_content))
     return response_content
 
 
-def MakeMessages(ask, message_history):
+def MakeMessages(ask: str, message_history: list[dict]) -> list[dict]:
     '''从聊天记录中生成ChatGPT的输入'''
 
     total_length = 0
@@ -38,7 +38,7 @@ def MakeMessages(ask, message_history):
         }
     ]
     for question, answer in reversed(message_history):
-        total_length += len(question) +len(answer)
+        total_length += len(question) + len(answer)
         if total_length > 2048:
             break
         reverse_message.append({
@@ -52,15 +52,13 @@ def MakeMessages(ask, message_history):
     return list(reversed(reverse_message))
 
 
-def handle_client(client_socket, addr, passwd):
+def handle_client(client_socket: socket.socket, addr: str, passwd: str) -> None:
     '''处理客户端连接'''
 
     message_history = []
 
-    print(f'客户端 {addr} 已连接，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
-
-
-    
+    print(
+        f'客户端 {addr} 已连接，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
 
     try:
         client_socket.send('请输入连接密码：'.encode())
@@ -69,24 +67,31 @@ def handle_client(client_socket, addr, passwd):
         if user_input_passwd.strip() != passwd:
             client_socket.send('密码错误，连接关闭\n'.encode())
             raise ValueError('密码错误')
-        
+
         client_socket.send('连接成功，开始聊天！(输入exit或quit来退出程序)\n'.encode())
         while True:
             client_socket.send('请输入你的问题：'.encode())
             data = client_socket.recv(4096).decode()
 
+            if data.strip() == "quit" or data.strip() == "exit":
+                raise ValueError('客户端主动断开连接')
+
             result = Chat(data, message_history)
 
             client_socket.send(('ChatGPT的回答:\n' + result + '\n').encode())
 
-            if data.strip() == "quit" or data.strip() == "exit":
-                raise ValueError('客户端主动断开连接')
     except ValueError as e:
-        print(f'客户端 {addr} {e}，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+        print(
+            f'客户端 {addr} {e}，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+    except ConnectionResetError as e:
+        print(
+            f'客户端 {addr} 异常断开连接，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
     except Exception as e:
-        print(f'客户端 {addr} {e}，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+        print(
+            f'客户端 {addr} {e}，时间：{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
 
-    print(f"客户端 {addr} 已断开连接，时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
+    print(
+        f"客户端 {addr} 已断开连接，时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
     client_socket.close()
 
 
@@ -107,7 +112,8 @@ def main():
     while True:
         client_socket, addr = server_socket.accept()
 
-        thread = threading.Thread(target=handle_client, args=(client_socket, addr, passwd))
+        thread = threading.Thread(
+            target=handle_client, args=(client_socket, addr, passwd))
         thread.start()
 
 
